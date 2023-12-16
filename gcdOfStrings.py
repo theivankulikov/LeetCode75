2f6b47169ae972c5e1835d1ad6d3b4c7b2ecd465
Ivan Kulikov <ivankulich@gmail.com>
2023-12-16 14:53:17 +0200

    Some refactoring

--- gcdOfStrings.py
+++ gcdOfStrings.py
@@ -33,5 +33,29 @@ class gcdOfStringsSolution:
         return True
     
 
-test_object = gcdOfStringsSolution()
-print(test_object.gcdOfStrings("abc", "ab"))
+
+def test_positive_values():
+    test_object = gcdOfStringsSolution()
+    assert test_object.gcdOfStrings("abcdef", "abcdef") == "abcdef"
+    assert test_object.gcdOfStrings("abcdefg", "abcdef") == ""
+    assert test_object.gcdOfStrings("abcdef", "abcdefg") == ""
+    assert test_object.gcdOfStrings("", "") == ""
+    assert test_object.gcdOfStrings("a", "") == ""
+    assert test_object.gcdOfStrings("", "a") == ""
+    assert test_object.gcdOfStrings("ab", "abab") == "ab"
+    assert test_object.gcdOfStrings("abab", "ab") == "ab"
+    assert test_object.gcdOfStrings("ab", "aba") == ""
+    assert test_object.gcdOfStrings("aba", "ab") == ""
+    assert test_object.gcdOfStrings("ababa", "ab") == ""
+    assert test_object.gcdOfStrings("ab", "ababa") == ""
+    assert test_object.gcdOfStrings("ababab", "abab") == "ab"
+
+def test_checkStringForDivider():
+    test_object = gcdOfStringsSolution()
+    assert test_object.checkStringForDivider("ab", "ab") == True
+    assert test_object.checkStringForDivider("abab", "ab") == True
+    assert test_object.checkStringForDivider("abc", "ab") == False
+    assert test_object.checkStringForDivider("ac", "ab") == False
+    assert test_object.checkStringForDivider("abababa", "ab") == False
+    assert test_object.checkStringForDivider("ac", "a") == False
+    assert test_object.checkStringForDivider("ab", "abab") == False
---
+++ kidsWithCandies.py
@@ -0,0 +1,14 @@
+"""
+https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+"""
+
+from typing import List
+
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        result = list()
+        return result
+    
+
+def test_Solution():
+    assert Solution().kidsWithCandies([2,3,5,1,3], 3) == [true,false,false,false,false]
