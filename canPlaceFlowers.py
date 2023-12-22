8cc53fb357b1a9489f777a2d75e62c1b3e8d2a19
Ivan Kulikov <ivankulich@gmail.com>
2023-12-22 18:08:51 +0200

    First working solution.
    Runtime: 136 ms, faster than 97.63% of Python3 online submissions for Can Place Flowers.
    Memory Usage: 17.9 MB, less than 13.79% of Python3 online submissions for Can Place Flowers.

--- canPlaceFlowers.py
+++ canPlaceFlowers.py
@@ -6,18 +6,19 @@ from typing import List
 
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        i = 1
+        flowerbed = [0] + flowerbed + [0]
+        i = 2
         while i < len(flowerbed):
-            if flowerbed[i - 1] == 0 and flowerbed[i] == 0:
+            if flowerbed[i - 2] == 0 and flowerbed[i - 1] == 0 and flowerbed[i] == 0:
                 n -= 1
                 i += 2
-            elif flowerbed[i - 1] == 1 and flowerbed[i] == 0:
+            elif flowerbed[i - 2] == 1:
+                i += 1
+            elif flowerbed[i - 1] == 1:
                 i += 2
             elif flowerbed[i] == 1:
                 i += 3
-            else:
-                i += 1
-        if n == 0:
+        if n <= 0:
             return True
         else:
             return False
