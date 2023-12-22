a471c6a52b9243c66f9c58dee14c4790a6ea9c28
Ivan Kulikov <ivankulich@gmail.com>
2023-12-22 18:14:36 +0200

    Optimization: exit while ASAP => faster than 99,94% of submissions Runtime: 126 ms, faster than 99.94% of Python3 online submissions for Can Place Flowers. Memory Usage: 17.9 MB, less than 13.79% of Python3 online submissions for Can Place Flowers.

--- canPlaceFlowers.py
+++ canPlaceFlowers.py
@@ -6,22 +6,23 @@ from typing import List
 
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        if n <= 0:
+            return True
         flowerbed = [0] + flowerbed + [0]
         i = 2
         while i < len(flowerbed):
             if flowerbed[i - 2] == 0 and flowerbed[i - 1] == 0 and flowerbed[i] == 0:
                 n -= 1
                 i += 2
+                if n <= 0:
+                    return True
             elif flowerbed[i - 2] == 1:
                 i += 1
             elif flowerbed[i - 1] == 1:
                 i += 2
             elif flowerbed[i] == 1:
                 i += 3
-        if n <= 0:
-            return True
-        else:
-            return False
+        return False
         
 
 def test_canPlaceFlowers():
