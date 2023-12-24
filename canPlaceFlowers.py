4402ad9f81462d88e7c2fccf3575664267045e22
Ivan Kulikov <ivankulich@gmail.com>
2023-12-24 14:01:45 +0200

    move tests to separate file (to pass through validation on github actions)

--- canPlaceFlowers.py
+++ canPlaceFlowers.py
@@ -25,37 +25,3 @@ class Solution:
             else:
                 i += 1
         return False
-    
-Solution().canPlaceFlowers(["a",0,0,1,0], 1)
-
-def test_canPlaceFlowers():
-    assert Solution().canPlaceFlowers([1,0,0,0,1], 1) == True
-    assert Solution().canPlaceFlowers([1,0,0,0,1], 2) == False
-    assert Solution().canPlaceFlowers([1,0,1,0,1], 1) == False
-    assert Solution().canPlaceFlowers([0,0,1,0,1], 1) == True
-    assert Solution().canPlaceFlowers([0,1,0,0,1], 1) == False
-    assert Solution().canPlaceFlowers([0,1,0,0,1], 2) == False
-    assert Solution().canPlaceFlowers([0,0,0,0,1], 2) == True
-    assert Solution().canPlaceFlowers([0,0,0,0,1], 3) == False
-    assert Solution().canPlaceFlowers([1,0,0,0,0], 1) == True
-    assert Solution().canPlaceFlowers([1,0,0,0,0], 2) == True
-    assert Solution().canPlaceFlowers([1,0,0,0,0], 3) == False
-    assert Solution().canPlaceFlowers([1,0,0,0,0,0], 3) == False
-    assert Solution().canPlaceFlowers([1,0,0,0,0,0], 2) == True
-    assert Solution().canPlaceFlowers([1,0], 1) == False
-    assert Solution().canPlaceFlowers([1,0], 0) == True
-    assert Solution().canPlaceFlowers([1,0,0], 1) == True
-    assert Solution().canPlaceFlowers([1,0,1], 1) == False
-    assert Solution().canPlaceFlowers([1,0,0,0], 1) == True
-    assert Solution().canPlaceFlowers([1,0,0,1], 1) == False
-    assert Solution().canPlaceFlowers([1,0,0,0,0], 1) == True
-    assert Solution().canPlaceFlowers([1,0,0,0,1], 1) == True
-    assert Solution().canPlaceFlowers([1,1,0,0,1], 1) == False
-    assert Solution().canPlaceFlowers([1,1,0,1,1], 1) == False
-    assert Solution().canPlaceFlowers([1,0,0,1,1], 1) == False
-    assert Solution().canPlaceFlowers([0,1,0,0,1], 1) == False
-    assert Solution().canPlaceFlowers([1,0,0,1,0], 1) == False
-    assert Solution().canPlaceFlowers(["a",0,0,1,0], 1) == True
-    assert Solution().canPlaceFlowers(["a",0,0,"a",0], 1) == True
-
-    
